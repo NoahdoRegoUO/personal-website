4ea928dd195e8a624b875d83511e1d6b3f2b8044
Noah do Rego <ndore051@uottawa.ca>
2023-06-27 22:41:54 -0400

    Changed title display settings

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,8 +2,8 @@ import { Typography } from "@material-tailwind/react";
 
 function Title(props: { text: string }) {
   return (
-    <div className="w-screen mt-10 mx-10">
-      <Typography variant="h1" className="text-base md:text-lg lg:text-2xl">
+    <div className="w-fill m-10">
+      <Typography variant="h1" className="text-center text-4xl lg:text-5xl">
         {props.text}
       </Typography>
     </div>

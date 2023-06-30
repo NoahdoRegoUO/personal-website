37b1b118661bb7599ec1f64836d86eed8d707e5c
Noah do Rego <ndore051@uottawa.ca>
2023-06-30 17:08:48 -0400

    Added unity project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,8 +2,14 @@ import bookerImage from "../assets/images/projects/booker.png";
 import onedayImage from "../assets/images/projects/oneday.png";
 import trendImage from "../assets/images/projects/trend.jpg";
 import personalWebsiteImage from "../assets/images/projects/personal-website.jpg";
+import unityImage from "../assets/images/projects/unity-art-2023.jpg";
 
 const projectsData = [
+  {
+    title: "Personal Website",
+    image: personalWebsiteImage,
+    desc: "My personal website (yes, this one!) made with Vite + React, Tailwind CSS",
+  },
   {
     title: "OneDay",
     image: onedayImage,
@@ -20,9 +26,9 @@ const projectsData = [
     desc: "A mobile app that displays trending content from other platforms made with React Native",
   },
   {
-    title: "Personal Website",
-    image: personalWebsiteImage,
-    desc: "My personal website (yes, this one!) made with Vite + React, Tailwind CSS",
+    title: "Unity Games",
+    image: unityImage,
+    desc: "All of the games I've made in Unity (programmed in C#)",
   },
 ];
 

321a2f1819727283babea2c271fc1d55f09d66fc
Noah do Rego <ndore051@uottawa.ca>
2023-06-30 23:54:53 -0400

    made component for contact icons

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,17 @@
  */
 
 // Pages
-export {default as HomePage} from "./pages/Home";
-export {default as ProjectsPage} from "./pages/Projects";
-export {default as BlogPage} from "./pages/Blog";
-export {default as GamesPage} from "./pages/Games";
-export {default as RecipesPage} from "./pages/Recipes";
+export { default as HomePage } from "./pages/Home";
+export { default as ProjectsPage } from "./pages/Projects";
+export { default as BlogPage } from "./pages/Blog";
+export { default as GamesPage } from "./pages/Games";
+export { default as RecipesPage } from "./pages/Recipes";
 
 // Components
-export {default as NavBar} from "./components/NavBar";
-export {default as Title} from "./components/Title";
-export {default as CardGrid} from "./components/CardGrid";
+export { default as NavBar } from "./components/NavBar";
+export { default as Title } from "./components/Title";
+export { default as CardGrid } from "./components/CardGrid";
+export { default as ContactIcons } from "./components/ContactIcons";
 
 // Data
-export {default as projectsData} from "./data/projects";
+export { default as projectsData } from "./data/projects";
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,6 @@
-import { Typography, IconButton } from "@material-tailwind/react";
+import { Typography } from "@material-tailwind/react";
 import { TypeAnimation } from "react-type-animation";
-import { FiGithub } from "react-icons/fi";
-import {
-  FaMediumM,
-  FaYoutube,
-  FaItchIo,
-  FaLinkedinIn,
-  FaEnvelope,
-} from "react-icons/fa";
+import { ContactIcons } from "../index.ts";
 
 function Home() {
   return (
@@ -39,36 +32,7 @@ function Home() {
             />
           </Typography>
           <div className="flex items-center gap-8 w-full justify-left ml-10 mt-5">
-            <a href="https://github.com/NoahdoRegoUO" target="_blank">
-              <IconButton className="rounded-full scale-[1.3] bg-[#000000] shadow-none">
-                <FiGithub className="h-[18px] w-[18px]" />
-              </IconButton>
-            </a>
-            <a href="https://www.linkedin.com/in/noah-do-rego/" target="_blank">
-              <IconButton className="rounded-full scale-[1.3] bg-[#3f7bdd] shadow-none">
-                <FaLinkedinIn className="h-[18px] w-[18px]" />
-              </IconButton>
-            </a>
-            <a href="https://www.youtube.com/@its_deego" target="_blank">
-              <IconButton className="rounded-full scale-[1.3] bg-[#c4302b] shadow-none">
-                <FaYoutube className="h-[18px] w-[18px]" />
-              </IconButton>
-            </a>
-            <a href="https://noah-dorego.medium.com/" target="_blank">
-              <IconButton className="rounded-full scale-[1.3] bg-[#000000] shadow-none">
-                <FaMediumM className="h-[18px] w-[18px]" />
-              </IconButton>
-            </a>
-            <a href="mailto:noah.dorego@gmail.com" target="_blank">
-              <IconButton className="rounded-full scale-[1.3] bg-[#6080ca] shadow-none">
-                <FaEnvelope className="h-[18px] w-[18px]" />
-              </IconButton>
-            </a>
-            <a href="https://itsdeego.itch.io/" target="_blank">
-              <IconButton className="rounded-full scale-[1.3] bg-[#d1604c] shadow-none">
-                <FaItchIo className="h-[18px] w-[18px]" />
-              </IconButton>
-            </a>
+            <ContactIcons />
           </div>
         </div>
         <div className="max-w-3xl mt-5 mx-10">

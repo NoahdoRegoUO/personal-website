dc31b282b70a188b315d26caf7ecf3ac949e2cd7
Noah do Rego <ndore051@uottawa.ca>
2023-07-13 23:01:25 -0400

    Added images + fixed links

--- src/data/games.ts
+++ src/data/games.ts
@@ -3,13 +3,14 @@ import gamePlaceholderImage from "../assets/images/games/game-placeholder.png";
 const gameData = [
   {
     title: "U.F.O.",
-    image: gamePlaceholderImage,
+    image: "https://img.itch.zone/aW1nLzg1NjI4MDgucG5n/508x254%23mb/SoOY5A.png",
     desc: "Play as an alien on a badly designed spaceship (Made for BR14 game jam)",
     link: "https://itsdeego.itch.io/ufo-unbearably-freaking-overwhelmed",
   },
   {
     title: "Rope Simulator",
-    image: gamePlaceholderImage,
+    image:
+      "https://manticoreprod-ms.azureedge.net/screenshots/b910434ec20e4ad4af4054257565fd80_0_35N17JrMvURlriSUCTRUIH_DBD11281AFF74669110982C70C88CDB94FAACCC9.jpg",
     desc: "An extension of the rope simulation program made by Sebastien Lague",
     link: "https://itsdeego.itch.io/rope-simulation",
   },
